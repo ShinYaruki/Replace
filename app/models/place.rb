b40faeda41d9a4eab4a0003b99868bce68464aff
Shin <5.1.1.cn7568m325373c@gmail.com>
2019-11-27 18:54:27 +0900

    ddd

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,5 +2,8 @@ class Place < ApplicationRecord
   has_many :place_ptags
   has_many :ptags, through: :place_ptags
 
+  validates :name, :address, :open, :close, :number,:station, presence: true
+  validates :name, :address, :number, uniqueness: true
+
   accepts_nested_attributes_for :place_ptags, allow_destroy: true
 end
