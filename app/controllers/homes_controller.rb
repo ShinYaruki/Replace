20905cf331f0beae1dd0a954e9956c2557f1f4db
Shin <5.1.1.cn7568m325373c@gmail.com>
2019-12-03 08:12:42 +0900

    cocococomit

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,10 +3,13 @@ class HomesController < ApplicationController
   def index
     @places = Place.all
     @musics = Music.all
+
     places = @places
     @myplaces = current_user.places
+
     musics = @musics
     @mymusics = current_user.musics
+
     @place = Place.new
     @music = Music.new 
   end
